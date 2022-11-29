c9e3e6d95960a571a784e7b7adeffb3b7d6d9d8e
EirikBergesen <46929671+EirikBergesen@users.noreply.github.com>
2022-11-29 23:20:55 +0100

    Update responding_server.py
    Changed functionallity to do some 2 way communication, and being able to ask for spesific file/picture

--- responding_server.py
+++ responding_server.py
@@ -9,17 +9,19 @@ class respondingServer:
     '''Server class
     Runs 'forever/until broken', and responds to requests by sending a file.
     '''
-    def __init__(self, host_ip, tcp_port, send_filename) -> None:
+    def __init__(self, host_ip, tcp_port, list_of_file_names) -> None:
         '''Takes params: (host ip, tcp port, filename to send from same directory)
         '''
         # Connection variables
         self.host_ip = host_ip
         self.tcp_port = tcp_port
         self.tcp_addr = (host_ip, tcp_port)
+        self._BUFFER_SIZE = 1024
+        self.tcp_transfer_endnote = 'FIN END'
 
         # Naming variables
         self.application_name = '[Sending Server]'
-        self.send_file_name = send_filename
+        self.list_of_file_names = list_of_file_names
         self.transfer_end_note = "FIN END"
 
         # Timezone variable
@@ -33,7 +35,7 @@ class respondingServer:
         '''Accept TCP connection and send data.
         Opens a socket create_server.
         Tries to find and read file from/in same directory as this script.
-        Packages file with acknowledgement message and an endnote.
+        Packages file, message and an endnote.
         Sends all.
         '''
         tcp_socket = create_server(self.tcp_addr)
@@ -42,49 +44,111 @@ class respondingServer:
         
         while True:
             conn, addr = tcp_socket.accept()
-
+            data = self._recvall(conn)
             # Start first timer
             first_time = self._get_time_from_timezone()
 
-            #print(f"{self.application_name} Sending: \nAcknowledgement message to address: {addr}. {self._get_time_from_timezone()}")
+            if type(data) == str and not data in self.list_of_file_names:
+                try:
+                    evaluated_data = eval(data)
+                except:
+                    print('The data received could not be interpreted')
+                if type(evaluated_data) == list:
+                    for entry in evaluated_data:
+                        if not entry in self.list_of_file_names:
+                            print('The sent list contains items that is not contained in the server repertoare.')
+                            return
+                    packet = self._make_data_package(eval(data), first_time=first_time)
+                    
 
-            # Try to find and read file
+            elif data in self.list_of_file_names:
+                packet = self._make_data_package(data, first_time=first_time)
+                
+            else:
+                print(data, 'is not in ', self.list_of_file_names)
+            
+            if packet:
+                msg = packet + self.transfer_end_note
+                conn.send(msg.encode())
+            else:
+                print('Could not build packet based on: ', str(data))
+
+
+    def _make_data_package(self, sending_filenames, first_time):
+        # Try to find and read file
+        if type(sending_filenames) == str: 
+            sending_filenames = [sending_filenames]
+
+        data = []
+        for iter, sending_filename in enumerate(sending_filenames):
             try:
                 path = getcwd()
-                with open(f"{path}/{self.send_file_name}", 'rb') as data_file:
-                    data = data_file.read()
-                    #print(f"{self.application_name} Sending:\n {self.send_file_name} \nto:  {addr}. {self._get_time_from_timezone()}")
+                full_path = f"{path}/{sending_filename}"
+                with open(full_path, 'rb') as data_file:
+                    data.append(data_file.read())
             except:
                 print(f"{self.application_name} The file does not exist. {self._get_time_from_timezone()}")
+                return
             
-            # Timer after reading
-            after_reading_time = self._get_time_from_timezone()
+        # Timer after reading
+        after_reading_time = self._get_time_from_timezone()
 
-            # Data management
-            after_data_operations_time = self._get_time_from_timezone()
+        # Data management
+        after_data_operations_time = self._get_time_from_timezone()
 
-            # Package timestamps
-            timestamps = dict()
-            timestamps['request_received'] = datetime.strftime(first_time, '%Y-%m-%d %H:%M:%S.%f%z')
-            timestamps['after_reading_data'] = datetime.strftime(after_reading_time, '%Y-%m-%d %H:%M:%S.%f%z')
-            timestamps['after_data_operations'] = datetime.strftime(after_data_operations_time, '%Y-%m-%d %H:%M:%S.%f%z')
+        # Package timestamps
+        timestamps = dict()
+        timestamps['request_received'] = datetime.strftime(first_time, '%Y-%m-%d %H:%M:%S.%f%z')
+        timestamps['after_reading_data'] = datetime.strftime(after_reading_time, '%Y-%m-%d %H:%M:%S.%f%z')
+        timestamps['after_data_operations'] = datetime.strftime(after_data_operations_time, '%Y-%m-%d %H:%M:%S.%f%z')
 
-
-            # Packaging and sending with end note
-            packet = str([data, str(timestamps)])
-            msg = packet + self.transfer_end_note
-            conn.send(msg.encode())
+        # Packaging
+        packet = str([data, str(timestamps)])
+        return packet
 
 
     def _get_time_from_timezone(self):
         '''Returns time.now() from spesific timezone (self.timezone).
         self.timezone was set to europe/oslo
         '''
-        time = datetime.now(pytimezone(self.timezone))
-        return time
+        return datetime.now(pytimezone(self.timezone))
+
+    
+    def _recvall(self, socket_connection):
+        '''Returns decoded message
+        Builds a byte array from several messages sendt, with size buffersize
+        '''
+        # Building byte array
+        msg_array = bytearray()
+        while True:
+            msg_part = socket_connection.recv(self._BUFFER_SIZE)
+            msg_array.extend(msg_part)
+
+            # Breaking at endnote
+            # TODO: Somewaht unsure if this is secure, or maybe slow
+            if str(msg_array.decode()).endswith(self.tcp_transfer_endnote):
+                break
+        
+        # Remove endnote
+        msg = msg_array.decode()[:-len(self.tcp_transfer_endnote)]
+        return msg
 
 
 tcp_port = 5500
 host_ip = ''
-send_file_name = 'tree-picture.jpg'
-server = respondingServer(host_ip, tcp_port, send_file_name)
+
+list_of_picture_names = [
+    'tree-picture_320240_00.png',
+    'tree-picture_320240_01.png',
+    'tree-picture_320240_02.png',
+    'tree-picture_320240_03.png',
+    'tree-picture_320240_04.png',
+    'tree-picture_320240_05.png',
+    'tree-picture_320240_06.png',
+    'tree-picture_320240_07.png',
+    'tree-picture_320240_08.png',
+    'tree-picture_320240_09.png'
+]
+
+
+server = respondingServer(host_ip, tcp_port, list_of_picture_names)
